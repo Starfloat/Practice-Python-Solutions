41610bfdd450f0c7312a150ebb6ecd20d72dac83
Starfloat <53842484+Starfloat@users.noreply.github.com>
2019-08-15 17:48:16 -0400

    Update 01-character-input.py

--- 01-character-input.py
+++ 01-character-input.py
@@ -1,1 +1,23 @@
+# https://www.practicepython.org/exercise/2014/01/29/01-character-input.html
 
+"""
+Exercise 1 (and Solution)
+Create a program that asks the user to enter their name and their age.
+Print out a message addressed to them that tells them the year that they will turn 100 years old.
+
+Extras:
+1. Add on to the previous program by asking the user for another number and printing out that many copies of the previous message.
+2. Print out that many copies of the previous message on separate lines.
+"""
+from datetime import date
+
+name = input('Enter your name: ')
+age  = int(input('Enter your age: '))
+
+today = date.today()
+year = today.year
+age_difference_from_100 = 100-age
+
+year_to_be_100 = year + age_difference_from_100
+
+print(name + ', you will be age 100 in the year', + year_to_be_100)
